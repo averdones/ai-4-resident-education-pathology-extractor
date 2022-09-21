3340d7b3341a127abcb06227083d94cf4192aa77
Antonio <averdones@gmail.com>
2022-09-21 16:45:03 -0400

    Add option to filter impressions my modality

--- src/data_preparation/save_impressions.py
+++ src/data_preparation/save_impressions.py
@@ -25,8 +25,12 @@ def create_impression(report: Report) -> list[str | float]:
     return [report.get_impression(), report.orig_acc, report.anon_acc, report.anon_acc_1, report.anon_acc_2]
 
 
-def create_impressions(reports: list[Report]) -> pd.DataFrame:
+def create_impressions(reports: list[Report], filter_by_modality: list[str] | None = None) -> pd.DataFrame:
     """Creates a Dataframe with only the impressions from a list of reports.
+
+    Args:
+        reports: List of Report objects.
+        filter_by_modality: List of modalities to filter the reports. If None, no filtering is done.
 
     Returns:
         Dataframe of Impressions and their corresponding accession numbers to keep track of them.
@@ -34,22 +38,27 @@ def create_impressions(reports: list[Report]) -> pd.DataFrame:
     """
     impressions = []
     for rep in reports:
-        impressions.append(create_impression(rep))
+        if filter_by_modality is None:
+            impressions.append(create_impression(rep))
+        else:
+            if rep.modality in filter_by_modality:
+                impressions.append(create_impression(rep))
 
     return pd.DataFrame(impressions, columns=["impression", "orig_acc", "anon_acc", "anon_acc_1", "anon_acc_2"])
 
 
-def save_impressions(body_section: str) -> None:
+def save_impressions(body_section: str, filter_by_modality: list[str] | None = None) -> None:
     """Saves the impression sections of the reports in a CSV files.
 
     Args:
         body_section: Body section to save the impressions from.
+        filter_by_modality: List of modalities to filter the reports. If None, no filtering is done.
 
     """
     reports, _ = load_reports_with_impression("src/data_preparation/data/merged_crosswalks_csv/sdr_crosswalks.csv",
                                               body_section=body_section)
 
-    impressions_df = create_impressions(reports)
+    impressions_df = create_impressions(reports, filter_by_modality)
 
     # Save to file
     impressions_df.to_csv(f"src/data_preparation/data/impressions/impressions_{body_section.lower()}.csv", index=False)
@@ -68,8 +77,12 @@ def main():
         BodySection.PEDS,
     ]
 
+    # Filter by modality
+    # filter_by_modality = ["mr"]
+    filter_by_modality = None
+
     for body_section in body_sections:
-        save_impressions(body_section)
+        save_impressions(body_section, filter_by_modality)
 
 
 if __name__ == '__main__':
